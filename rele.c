779003937b8f0b19aa0cac866f02280553e7582d
Jailsonmc <jailsonmc@gmail.com>
2018-06-18 19:09:56 +0100

    Add commentaries
    Add commentaries

--- rele.c
+++ rele.c
@@ -21,23 +21,23 @@ void loop() {
   while (Serial.available()==0){                                     
     //Wait for user input  
   }  
-  current = Serial.parseFloat();
-  Serial.print("Corrent actual:");
-  Serial.println(current);
+  current = Serial.parseFloat(); // recebe o valor digitado e tenta converter para numero com virgula
+  Serial.print("Corrent actual:"); // escreve texto
+  Serial.println(current); // escreve o valor convertido para numero com virgula
 
-   if(current > 5){
+   if(current > 5){ // verifica se e maior que 5 
      Serial.println("Fase 2. Current above 5 A: ");
      Serial.println("Enter a new current: "); 
-     delay(200);
+     delay(200); // espera 200 ms 
      
      while (Serial.available()==0)  {                                       
        //Wait for user input  
      }
-     current = Serial.parseFloat();
-     Serial.print("Corrent actual:");
-     Serial.println(current);
+     current = Serial.parseFloat();// recebe o valor digitado e tenta converter para numero com virgula
+     Serial.print("Corrent actual:");// escreve texto
+     Serial.println(current);// escreve o valor convertido para numero com virgula
      
-     if(current > 5){
+     if(current > 5){ // verifica se e maior que 5 
       
        Serial.println("Fase 3. Current above 5A for the second time: ");
        Serial.println("Enter a new current: "); 
@@ -50,9 +50,9 @@ void loop() {
        Serial.print("Corrent actual:");
        Serial.println(current);
        
-       digitalWrite(ledGreen, LOW); // sets the LED to the button's value
-       digitalWrite(ledRed, HIGH); // Abre o circuito
-       delay(1000);
+       digitalWrite(ledGreen, LOW); // seta o led verde desligado
+       digitalWrite(ledRed, HIGH); // seta o led vermelho ligado
+       delay(1000); // espera 1000 ms , 1s
 
        //while(cont == 1){
        
@@ -61,23 +61,24 @@ void loop() {
            Serial.println("Fase 4. Current above 5A for the third time, manual reclosing required. ");
            Serial.println("Push the button: "); 
           
-           while(digitalRead(abreCircuito) == LOW) { }
+           while(digitalRead(abreCircuito) == LOW) { // enquanto o botao n emitir sinal fica em um ciclo
+           }
 
-           if(digitalRead(abreCircuito) == HIGH){
-             Serial.println("Reopened circuit: ");
-             Serial.println("Program closed: ");
+           if(digitalRead(abreCircuito) == HIGH){ // caso o botao emita sinal entra nessa condicao
+             Serial.println("Reopened circuit: "); // escreve testo
+             Serial.println("Program closed: "); // escreve testo
 
-             digitalWrite(ledGreen, HIGH); // sets the LED to the button's value
-             digitalWrite(ledRed, LOW); // Abre o circuito
+             digitalWrite(ledGreen, HIGH); // sets o led verde ligado
+             digitalWrite(ledRed, LOW); // sets o led vermelho desligado
            }
            
-           delay(1000); 
-           cont = 1;
+           delay(1000);  // espera 1s
+           cont = 1; // seta variavel cont igual a 1
            
            // ativar botão - colocar um botão para religar
 
          }else{
-           cont = 0;
+           cont = 0; // seta variavel cont igual a 0
            Serial.println("menor que 5"); 
            delay(500);
          }
